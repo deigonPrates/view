c23b7fa15df2d0525faf36668584928c1d4a4219
Deigon Prates <deigon_prates@hotmail.com>
2017-10-09 23:27:46 -0300

    Melhoria
    velocidade de locomoção normal incompatível com 80 cm, deficiente se choca ao objeto

--- projetoSensor.ino
+++ projetoSensor.ino
@@ -74,7 +74,7 @@ void sensorUltrassonico() {
   Serial.print(distancia_cm);
   Serial.println(" cm");
 
-  if (distancia_cm <= 80 && distancia_cm > 3) {
+  if (distancia_cm <= 140 && distancia_cm > 3) {
     buzzer();
     coordenadasGPS();
   }
